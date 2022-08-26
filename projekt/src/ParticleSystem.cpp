fc7814d10775a0b3177c195a503d445c115fce46
maxono1 <92861475+maxono1@users.noreply.github.com>
2022-08-26 17:26:31 +0200

    small commit

--- projekt/src/ParticleSystem.cpp
+++ projekt/src/ParticleSystem.cpp
@@ -6,3 +6,15 @@ void ParticleSystem::emit(const ParticleProps& particleProps)
 {
     // TODO: Add your implementation code here.
 }
+
+
+void ParticleSystem::update(float dtime)
+{
+    // TODO: Add your implementation code here.
+}
+
+
+void ParticleSystem::draw(const BaseCamera& cam)
+{
+    // TODO: Add your implementation code here.
+}
